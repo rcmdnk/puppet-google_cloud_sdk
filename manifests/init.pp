ad081bdbe7844853bfc61afbd2f37806ef9a10c1
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2018-12-04 18:57:38 +0900

    added GCP setup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,19 @@
-class google_cloud_sdk {
+class google_cloud_sdk (
+  $service_account_file = "",
+  $user = "root",
+  $tmp_file = "/tmp/service_account_file",
+  $project = "",
+  $zone = "",
+){
   class { 'google_cloud_sdk::repositories': }
   class { 'google_cloud_sdk::install': }
+  if $service_account_file != "" {
+    class { 'google_cloud_sdk::service_account':
+      service_account_file => $service_account_file,
+      tmp_file => $tmp_file,
+      user => $user,
+      project => $project,
+      zone => $zone,
+    }
+  }
 }
---
+++ manifests/service_account.pp
@@ -0,0 +1,32 @@
+class google_cloud_sdk::service_account (
+  $service_account_file = "",
+  $tmp_file = "/tmp/service_account_file",
+  $user = "root",
+  $project = "",
+  $zone = "",
+){
+  if $service_account_file != "" {
+    file {service_account_file:
+      path => $tmp_file,
+      source => $service_account_file,
+      owner => $user,
+      mode => 600,
+    }
+    exec {"GCP account setting":
+      command => "/usr/bin/sudo -u ${user} /usr/bin/gcloud auth activate-service-account --key-file ${tmp_file}",
+    }
+    if $project != "" {
+      exec {"GCP project setting":
+        command => "/usr/bin/sudo -u ${user} /usr/bin/gcloud config set project ${project}",
+      }
+    }
+    if $zone != "" {
+      exec {"GCP zone setting":
+        command => "/usr/bin/sudo -u ${user} /usr/bin/gcloud config set compute/zone ${zone}",
+      }
+    }
+    exec {"GCP account file deletion":
+      command => "/usr/bin/sudo -u ${user} rm -f ${tmp_file}",
+    }
+  }
+}
